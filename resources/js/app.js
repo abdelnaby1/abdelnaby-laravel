2d42753878372080785d49f2ad33ff5976ef0d87
abdelnaby <ahmedabdelnaby471@gmail.com>
2020-04-05 00:45:22 +0200

    Configured Js and Css and set up Database

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
+
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+import Buefy from 'buefy'
+
+Vue.use(Buefy)
+
+
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+// const app = new Vue({
+//     el: '#app',
+// });
